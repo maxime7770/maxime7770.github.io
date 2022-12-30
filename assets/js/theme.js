bd3001b05390fbb97583a479e4082c459ad33c47
Intern <maxwolf34@gmail.com>
2022-12-30 15:31:37 +0400

    test

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -71,8 +71,8 @@ let transTheme = () => {
 let initTheme = (theme) => {
   if (theme == null || theme == 'null') {
     const userPref = window.matchMedia;
-    if (userPref && userPref('(prefers-color-scheme: dark)').matches) {
-        theme = 'dark';
+    if (userPref && userPref('(prefers-color-scheme: ligt)').matches) {
+        theme = 'light';
     }
   }
 
